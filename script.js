d0f19b8c44b13399016a7f068e5be8cec4c62b28
unknown <estudiante@LAB0272.SERVERINT.LOCAL>
2017-03-07 12:30:42 -0600

    examen

---
+++ script.js
@@ -0,0 +1,9 @@
+var bacoA = angular.module("bacoA",[]);
+bacoA.controller("mensaje",function($scope){
+	$scope.texto = "fernando251"
+	 });
+
+
+bacoA.controller("nombre",function($scope){
+    
+});
